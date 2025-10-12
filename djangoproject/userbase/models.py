ce5a98c30289b603aff404e85369cbaa96b4d7df
twistedttea <twistedtteavr@gmail.com>
2025-10-12 17:33:06 -0500

    feat: User model
    Initial structure for a user class utilizing the ORM django model

---
+++ djangoproject/userbase/models.py
@@ -0,0 +1,16 @@
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    location = ...
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = true
+
+
+class Walker(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
