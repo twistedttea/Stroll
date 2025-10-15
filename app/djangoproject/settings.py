b4f3a534d0e63bbe4f01d7c346c0575173c491d6
twistedttea <twistedtteavr@gmail.com>
2025-10-14 23:48:22 -0500

    feat: docker
    Fix database env vars not being imported
    Fix staticfiles

--- app/djangoproject/settings.py
+++ app/djangoproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,8 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = "/app/staticfiles"
 
+# The URL Django will use to serve the files (often /static/)
+STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
